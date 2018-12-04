60569145693c4d36a0d4c4f5b56e0bde7e95773c
alexandrbublik <alexandrbublik@gmail.com>
2018-12-04 16:57:35 +0200

    end while loop task

--- while-loop/task.js
+++ while-loop/task.js
@@ -1,18 +1,30 @@
 // use: while loop
 // don't use: *, / and %
 function getReminder(dividend, divider) {
-  return 2;
+  while (dividend >= divider){
+    dividend = dividend - divider;
+  }
+  return dividend;
 }
 
 // use: while loop
 // don't use: *, / and %
 function integerDivision(dividend, divider) {
-  return 2;
+  var res = 1;
+  while (dividend - divider >= divider){
+    dividend = dividend - divider;
+    res++;
+  }
+  return res;
 }
 
 // * bank deposit calculator
 function getDepositDuration(initialDeposit, monthlyPercent, desiredDeposit) {
   var monthCounter = 0;
-
+  var incomeDeposit = initialDeposit;
+  while(desiredDeposit > incomeDeposit){
+    incomeDeposit += incomeDeposit*(monthlyPercent/100);
+    monthCounter++;
+  }
   return monthCounter;
 }
